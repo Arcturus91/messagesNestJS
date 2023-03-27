3abcde5a795d08c601d0debb536a6a1750f35d62
Arcturus91 <arturo.barrantesv@pucp.pe>
2023-03-27 01:00:42 -0500

    Implement dependency injection

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,13 +1,41 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
+
+
+  constructor(public messagesService: MessagesService) {
+    
+  }
+
   @Get()
-  listMessages() {}
-  
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+
+    return this.messagesService.create(body.content);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  async getMessage(@Param('id') id: string) {
+    console.log(id);
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+    return message;
+  }
 }
